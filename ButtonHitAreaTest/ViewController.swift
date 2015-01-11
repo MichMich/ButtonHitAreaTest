ab344b757e86d01b836d03615e44a63759d46dff
Michael Teeuw <michael@xonaymedia.nl>
2015-01-11 10:41:05 +0100

    Initial Commit

---
+++ ButtonHitAreaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ButtonHitAreaTest
+//
+//  Created by Michael Teeuw on 11-01-15.
+//  Copyright (c) 2015 Michael Teeuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
